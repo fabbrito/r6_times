8ee712bc34481242f92bbc1350dad2eb0a71a4f8
Fabrício <fab.brito94@gmail.com>
2020-02-10 10:00:46 -0300

    Troca de XMLHttpRequest para fetch

--- js/busca_r6tab.js
+++ js/busca_r6tab.js
@@ -13,48 +13,40 @@ botaoBusca.addEventListener("click", function(event) {
 });
 
 //=============================================================
-function apiSearchRequest(jogador) {
-  var xhr = new XMLHttpRequest();
-  var apiSearchURL = "https://r6tab.com/api/search.php?platform=uplay&search=" + jogador.querySelector(".info-nick").textContent;
-  xhr.open("GET", apiSearchURL);
+function erroBuscaJogador(jogador) {
+  jogador.classList.add("erro-jogador");
+  var msgErro = "Um dos jogadores não foi localizado na base de dados do R6 Tab!";
+  exibeMensagensDeErro([msgErro], "#erros-busca");
+};
 
-  xhr.onload = function() {
-    if (xhr.status == 200) {
-      var r6TabResp = JSON.parse(this.responseText);
-      if (Object.entries(r6TabResp).length > 1) {
-        var results = r6TabResp.results;
-        jogador.querySelector(".info-nick").textContent = results[0].p_name;
-        // jogador.querySelector(".info-mmr").textContent = results[0].p_currentmmr;
-        apiPlayerRequest(jogador, results[0].p_id);
-      };
-      if (Object.entries(r6TabResp).length === 1) {
-        jogador.classList.add("erro-jogador");
-        var msgErro = "Um dos jogadores não foi localizado na base de dados do R6 Tab!";
-        exibeMensagensDeErro([msgErro], "#erros-busca");
-      };
-    };
-  };
-  xhr.send();
+//=============================================================
+function apiSearchRequest(jogador) {
+  const apiSearchURL = "https://r6tab.com/api/search.php?platform=uplay&search=" + jogador.querySelector(".info-nick").textContent;
+  fetch(apiSearchURL)
+    .then(res => res.json())
+    .then(res => {
+      jogador.querySelector(".info-nick").textContent = res.results[0].p_name;
+      // jogador.querySelector(".info-mmr").textContent = results[0].p_currentmmr;
+      apiPlayerRequest(jogador, res.results[0].p_id);
+    })
+    .catch(err => {
+      erroBuscaJogador(jogador);
+      console.log(`Error with message: ${err}`)
+    });
 };
 
 //=============================================================
 function apiPlayerRequest(jogador, p_id) {
-  var xhr = new XMLHttpRequest();
-  var apiSearchURL = "https://r6tab.com/api/player.php?p_id=" + p_id;
-  xhr.open("GET", apiSearchURL);
-  xhr.onload = function() {
-    if (xhr.status == 200) {
-      var r6TabResp = JSON.parse(this.responseText);
-      if (r6TabResp.playerfound) {
-        jogador.querySelector(".info-mmr").textContent = r6TabResp.seasonal.current_NA_mmr;
-      } else {
-        jogador.classList.add("erro-jogador");
-        var msgErro = "Um dos jogadores não foi localizado na base de dados do R6 Tab!";
-        exibeMensagensDeErro([msgErro], "#erros-busca");
-      };
-    };
-  };
-  xhr.send();
+  const apiPlayerURL = "https://r6tab.com/api/player.php?p_id=" + p_id;
+  fetch(apiPlayerURL)
+    .then(res => res.json())
+    .then(res => {
+      jogador.querySelector(".info-mmr").textContent = res.seasonal.current_NA_mmr
+    })
+    .catch(err => {
+      erroBuscaJogador(jogador);
+      console.log(`Error with message: ${err}`)
+    });
 };
 
 //=============================================================
