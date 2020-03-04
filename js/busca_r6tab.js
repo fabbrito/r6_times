ea632a7404a7b14cc42fcfba2b9591e3bd19489d
Fabrício <fab.brito94@gmail.com>
2020-03-03 21:20:37 -0300

    Testes iniciais com um spinner

--- js/busca_r6tab.js
+++ js/busca_r6tab.js
@@ -2,9 +2,17 @@ var botaoBusca = document.querySelector("#buscar-jogadores");
 
 botaoBusca.addEventListener("click", function(event) {
   var listaJogadores = document.querySelectorAll(".jogador");
-  listaJogadores.forEach(jogador => {
+
+  listaJogadores.forEach((jogador) => {
     if (jogador.querySelector(".info-mmr").textContent.length === 0) {
-      apiSearchRequest(jogador);
+      addSpinner();
+      return;
+    };
+  });
+
+  listaJogadores.forEach((jogador, i) => {
+    if (jogador.querySelector(".info-mmr").textContent.length === 0) {
+      apiSearchRequest(jogador, i, listaJogadores.length);
     };
   });
 
@@ -15,14 +23,14 @@ botaoBusca.addEventListener("click", function(event) {
 });
 
 //=============================================================
-function apiSearchRequest(jogador) {
+function apiSearchRequest(jogador, i, quantJog) {
   const apiSearchURL = "https://r6tab.com/api/search.php?platform=uplay&search=" + jogador.querySelector(".info-nick").textContent;
   fetch(apiSearchURL)
     .then(res => res.json())
     .then(res => {
       jogador.querySelector(".info-nick").textContent = res.results[0].p_name;
       // jogador.querySelector(".info-mmr").textContent = results[0].p_currentmmr;
-      apiPlayerRequest(jogador, res.results[0].p_id);
+      apiPlayerRequest(jogador, i, quantJog, res.results[0].p_id);
     })
     .catch(err => {
       erroBuscaJogador(jogador);
@@ -31,7 +39,7 @@ function apiSearchRequest(jogador) {
 };
 
 //=============================================================
-function apiPlayerRequest(jogador, p_id) {
+function apiPlayerRequest(jogador, i, quantJog,  p_id) {
   const apiPlayerURL = "https://r6tab.com/api/player.php?p_id=" + p_id;
   fetch(apiPlayerURL)
     .then(res => res.json())
@@ -41,6 +49,10 @@ function apiPlayerRequest(jogador, p_id) {
     .catch(err => {
       erroBuscaJogador(jogador);
       console.log(`Error with message: ${err}`)
+    })
+    .then(() => {
+      if (i === (quantJog - 1)) return removeSpinner()
+      else return;
     });
 };
 
---
+++ js/spinner.js
@@ -0,0 +1,12 @@
+var spinner = document.querySelector("#spinner");
+
+function addSpinner() {
+  spinner.classList.add("spinner-border");
+  spinner.classList.add("text-danger")
+
+};
+
+function removeSpinner() {
+  spinner.classList.remove("spinner-border");
+  spinner.classList.remove("text-danger")
+};
